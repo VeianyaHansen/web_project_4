fef1ab7d3687493e2d6f1630dc914224fa7bd600
Veianya Hansen <melaya0105@gmail.com>
2020-08-28 22:50:21 -0700

    refactor: removed fixed width and height properties

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,23 @@ const editButton = document.querySelector('.button__edit');
 const closeButton = document.querySelector('.modal__close-button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.modal__form');
-const page = document.querySelector('.page');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputName = document.querySelector('.modal__form-input_type_name');
+  const inputAboutMe = document.querySelector('.modal__form-input_type_about-me');
+
+  const profileName = document.querySelector('.profile__heading');
+  const profileAboutMe = document.querySelector('.profile__text');
+
+  profileName.textContent = inputName.value;
+  profileAboutMe.textContent - inputAboutMe.value;
+
+  toggleModal();
+})
 
 function toggleModal() {
-  //page.classList.toggle('shadow');
   modal.classList.toggle('modal_is-open');
 }
 
@@ -22,18 +34,5 @@ closeButton.addEventListener('click', () => {
   
 })
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
 
-  const inputName = document.querySelector('.modal__form-input_type_name');
-  const inputAboutMe = document.querySelector('.modal__form-input_type_about-me');
-
-  const profileName = document.querySelector('.profile__heading-text_type_name');
-  const profileAboutMe = document.querySelector('.profile__text_type_name');
-
-  profileName.textContent = inputName.value;
-  profileAboutMe.textContent - inputAboutMe.value;
-
-  toggleModal();
-})  
   
