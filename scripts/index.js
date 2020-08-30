df9c59b253ac95ad4894a21c80a7fcc3efea345c
Veianya Hansen <melaya0105@gmail.com>
2020-08-30 16:05:16 -0700

    fix: click handler function

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,35 @@
-const editButton = document.querySelector('.button__edit');
-const closeButton = document.querySelector('.modal__close-button');
-const modal = document.querySelector('.modal');
-const form = document.querySelector('.modal__form');
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".modal__close-button");
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".modal__form");
+const inputName = form.querySelector(".modal__form-input_type_name");
+const inputAboutMe = form.querySelector(".modal__form-input_type_about-me");
+const profileName = document.querySelector(".profile__heading");
+const profileAboutMe = document.querySelector(".profile__text");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const inputName = document.querySelector('.modal__form-input_type_name');
-  const inputAboutMe = document.querySelector('.modal__form-input_type_about-me');
-
-  const profileName = document.querySelector('.profile__heading');
-  const profileAboutMe = document.querySelector('.profile__text');
-
   profileName.textContent = inputName.value;
-  profileAboutMe.textContent - inputAboutMe.value;
-
+  profileAboutMe.textContent = inputAboutMe.value;
+  // = is
+  // == is equal
+  // === is exactly equal
   toggleModal();
-})
+});
 
 function toggleModal() {
-  modal.classList.toggle('modal_is-open');
+  modal.classList.toggle("modal_is-open"); // add, remove, toggle
 }
 
-editButton.addEventListener('click', toggleModal);
+editButton.addEventListener("click", toggleModal);
 
-closeButton.addEventListener('click', toggleModal);
+closeButton.addEventListener("click", toggleModal);
 
-closeButton.addEventListener('click', () => {
+/*closeButton.addEventListener('click', () => {
   
-  document.querySelector('.modal__form-input_type_name').value = "";
+  inputName.value = "";
   
-  document.querySelector('.modal__form-input_type_about-me').value = "";
+   inputAboutMe.value = "";
   
-})
-
-
-  
+})*/
